ac988a768e4aae27b6357f922521ed18f42bbab2
Brooke <brooke.robinson@colorado.edu>
2015-09-04 09:03:16 -0600

    Halfway through Tree

--- Robinson_Assignment1.py
+++ Robinson_Assignment1.py
@@ -52,54 +52,151 @@ class MyNode:
 		self.l = l
 		self.r = r
 		self.p = p
+		
+	def getChildren(self):
+		return [self.l, self.r]
 			
 class MyTree:
 	def __init__(self, rootkey):
 
 		self.root = MyNode(rootkey, None, None, None)
 			
+	def checkTree(self, value, parentValue, root):
+		if root == None:
+			return False
+		if root.intkey == parentValue:
+			if root.l == None:
+				newNode = MyNode(value, None, None, root)
+				root.l = newNode
+				print("Added: ", value," Under Parent: ",root.intkey)
+				return newNode
+			elif root.r == None:
+				newNode = MyNode(value, None, None, root)
+				root.r = newNode
+				print("Added: ", value," Under Parent: ",root.intkey)
+				return newNode
+			else:
+				print("Parent has two children, node not added.")
+				return False
+		else:
+			for child in root.getChildren():
+				add_temp = self.checkTree(value, parentValue, child)
+				if add_temp:
+					return add_temp
+		
+		
 	def add(self, value, parentValue):
-		#search for parentValue
-		if parentValue > self.root.intkey:
-			#go right
-			add(self, parentValue, self.root.r.intkey)
-		if parentValue < self.root.intkey:
-			add(self, parentValue, self.root.l.intkey)
-		if parentValue == self.root.intkey:
+		if self.root == None:
+			print("Added new root")
+			newNode = MyNode(value, None, None, None)
+			self.root = newNode
+		if self.root.intkey == parentValue:
+			if self.root.l == None:
+				newNode = MyNode(value, None, None, self.root)
+				self.root.l = newNode
+				print("Added: ", value," Under Parent: ",self.root.intkey)
+				return True
+			elif self.root.r == None:
+				newNode = MyNode(value, None, None, self.root)
+				self.root.r = newNode
+				print("Added: ", value," Under Parent: ",self.root.intkey)
+				return True
+			else:
+				print("Parent has two children, node not added.")
+				return False
+		else:
+			#recursively search through children
+			for child in self.root.getChildren():
+				add_node = self.checkTree(value, parentValue, child)
+				if add_node:
+					return add_node
+		print("Node not found")
+		return False
+	
+	def findNodeDelete(self, value, root):
+		if root == None:
+			return False
+		if value == root.intkey:
+			if root.l == None and root.r == None:
+				print("Deleting Node", root.intkey)
+				#update parent
+				if root.p.l.intkey == value:
+					root.p.l = None
+				elif root.p.r.intkey == value:
+					root.p.r = None
+				root = None
+				return True
+			else:
+				print("Node not deleted, has children")
+				return False
+		else:
+			for child in root.getChildren():
+				delete_node = self.findNodeDelete(value, child)
+				if delete_node:
+					return delete_node
 			
 		
 		
-		
-		
-		#if (value > self.root.intkey):
-			##go right
-			#if(self.root.r == None):
-				#newNode = MyNode(value, None, None, self.root)
-				#self.root.r = newNode
-			#else:
-				#add(self, value, root.rc.intkey)
-				
-		#if (value < self.root.intkey):
-			##go left
-			#if(self.root.l == None):
-				#newNode = MyNode(value, None, None, self.root)
-				#self.root.l = newNode
-	
-
 	def delete(self, value):
-		self.intkey = value #have to search to see if it's in there
-		if (self.r != 0 ) or (self.l != 0):
-			print("Parent has two children, node not added")
+		if value == self.root.intkey:
+			if self.root.l == None and self.root.r == None:
+				print("Deleting Root")
+				self.root = None
+				return True
+			else:
+				print("Node not deleted, has children")
+				return False
 		else:
-			print("Node not found")
+			for child in self.root.getChildren():
+				delete_node = self.findNodeDelete(value, child)
+				if delete_node:
+					return delete_node
+					
+		print("Node not found")
+		return False
+		
 	def printTree(self):
-		print("Parent Key Value: ",self.p)
-		print("Left Key Value: ", self.l, "Right Key Value: ", self.r)
+		if self.root != None:
+			print (self.root.intkey)
+			for child in self.root.getChildren():
+				self.printBranch(child)
+		else: 
+			return
+			
+	
+	def printBranch(self, root):
+		if root == None:
+			return
+		else:
+			print (root.intkey)
+			for child in root.getChildren():
+				self.printBranch(child)
+					
+	
+				
 		
 #Tree Test
 
-root = MyNode(5, 4, 6, None)
-tree = MyTree(root, True)
+
+tree = MyTree(5)
+tree.add(6,5)
+tree.add(4,5)
+tree.add(7,4)
+tree.add(3,7)
+tree.add(8,4)
+tree.add(2,8)
+tree.add(9,7)
+tree.add(1,3)
+tree.add(10,3)
+
+tree.printTree()
+
+tree.delete(10)
+tree.delete(1)
+
+tree.add(18,3)
+
+tree.printTree()
 
 	
 
