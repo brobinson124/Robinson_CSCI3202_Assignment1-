46dedd58b86c38f6b48d6a4d75efc65537e8aa8f
Brooke <brooke.robinson@colorado.edu>
2015-09-04 11:04:17 -0600

    Completed

--- Robinson_Assignment1.py
+++ Robinson_Assignment1.py
@@ -1,10 +1,13 @@
 #Assignment1
+#Brooke Robinson
 
 
 #Problem 1
 #impliment a queue
 
-import Queue
+import Queue 
+#python2 is 'Queue'
+#python3 is 'queue'
 
 q = Queue.Queue()
 test = [1,2,3,4,5,6,7,8,9,10]
@@ -14,9 +17,8 @@ for x in test:
 	else:
 		print ('Ints')
 		quit()
-while q.qsize() > 0:
-	print 'D-qed: ',q.get()
 
+''''''''''''''''''''''''''''''''''''''''''
 #Problem 2 
 #implement a stack
 
@@ -32,20 +34,14 @@ class mystack:
 	def checkSize(self):
 		return len(self.data)
 
-#stack test
-stacky = mystack([])
-test = [1,2,3,4,5,6,7,8,9,10]
-for x in test:
-    stacky.push(x)
-y = 0
-while stacky.checkSize() > 0:
-	print stacky.data[(stacky.checkSize()-1)]
-	stacky.pop()
+''''''''''''''''''''''''''''''''''''''''''
 
 #Problem 3
 #Make a Tree
 
 class MyNode:
+	#create a Node class that contains value, leftchild, rightchild,
+	#and parent attributes.
   
 	def __init__(self, intkey, l, r, p):
 		self.intkey = intkey
@@ -54,15 +50,20 @@ class MyNode:
 		self.p = p
 		
 	def getChildren(self):
+		#returns both left and right children
 		return [self.l, self.r]
 			
 class MyTree:
 	def __init__(self, rootkey):
-
 		self.root = MyNode(rootkey, None, None, None)
+		#create a new tree while setting root
 			
 	def checkTree(self, value, parentValue, root):
+		#Recursive function that searches through tree to find
+		#if parentValue exists
+		
 		if root == None:
+			#if there is no root in tree
 			return False
 		if root.intkey == parentValue:
 			if root.l == None:
@@ -174,11 +175,72 @@ class MyTree:
 			for child in root.getChildren():
 				self.printBranch(child)
 					
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 	
-				
+#Number 4
+#Bring on the Graph!
+#Unweighted
 		
+class graph:
+	def __init__(self):
+		self.verticies = {}
+			
+	def addVertex(self, value):
+		#check if value already exists
+		if value in self.verticies:
+			print "Vertex already exists"
+		else:
+			self.verticies[value] = []
+			
+	def addEdge(self, value1, value2):
+		if value1 in self.verticies and value2 in self.verticies:
+			self.verticies[value1].append(value2)
+			self.verticies[value2].append(value1)
+		else:
+			print "One or more vertices not found."
+			
+	def findVertex(self, value):
+		if value in self.verticies:
+			adj = self.verticies[value]
+			print adj
+		else:
+			print "Not found."
+			
+'''''''''''''''''''''''''''''''''''''''''''''''''''
+Tests
+'''''''''''''''''''''''''''''''''''''''''''''''''''
+
+#Queue Test
+
+print "Queue Test: "
+print "Enqueue and Dequeue 10 ints"
+print ""
+while q.qsize() > 0:
+	print 'D-qed: ',q.get()
+
+
+#stack test
+
+print "-------------------------------------------"
+print "Stack Test: "
+print "Pop 10 ints"
+print ""
+stacky = mystack([])
+test = [1,2,3,4,5,6,7,8,9,10]
+for x in test:
+    stacky.push(x)
+y = 0
+while stacky.checkSize() > 0:
+	print stacky.data[(stacky.checkSize()-1)]
+	stacky.pop()
+	
+	
 #Tree Test
 
+print "-------------------------------------------"
+print "Tree Test"
+print "add 10 ints to tree, print In-Order, delete 2 ints, print In-Order"
+print ""
 
 tree = MyTree(5)
 tree.add(6,5)
@@ -191,7 +253,11 @@ tree.add(9,7)
 tree.add(1,3)
 tree.add(10,3)
 
+print ""
+
 tree.printTree()
+
+print ""
 
 tree.delete(10)
 tree.delete(1)
@@ -200,37 +266,51 @@ tree.add(18,3)
 
 tree.printTree()
 
+#Graph Test
 
-#Number 4
-#Bring on the Graph!
-#Unweighted
-		
-class graph:
-	def __init__(self):
-		self.adj = {}
-		
-			
-	def addVertex(self, value):
-		#check if value already exists
-		if value in self.adj:
-			print "Vertex already exists"
-		else:
-			self.adj[value] = []
-			
-	def addEdge(value1, value2):
-		if value1 in self.adj and value2 in self.adj:
-			value1 = value1.adj[value2]
-			value2 = value2.adj[value1]
-			
-		else:
-			print "One or more vertices not found."
-			
-		
-	
-		
-		
+print "-------------------------------------------"
+print "Graph Test"
+print "Add 10 vertecies, make 20 edges, print edges of five vertecies"
+print ""
+
 g = graph()
-g.addVertex(5)
-g.addVertex(4)
+g.addVertex(1)
+g.addVertex(11)
+g.addVertex(12)
+g.addVertex(13)
+g.addVertex(14)
+g.addVertex(15)
+g.addVertex(16)
+g.addVertex(17)
+g.addVertex(18)
+g.addVertex(19)
+g.addVertex(100)
 
-		
+g.addEdge(1,12)
+g.addEdge(1,13)
+g.addEdge(11,14)
+g.addEdge(15,11)
+g.addEdge(16,100)
+g.addEdge(15,17)
+g.addEdge(15,12)
+g.addEdge(12,13)
+g.addEdge(12,14)
+g.addEdge(12,16)
+g.addEdge(12,17)
+g.addEdge(1,100)
+g.addEdge(12,100)
+g.addEdge(15,100)
+g.addEdge(19,12)
+g.addEdge(13,100)
+g.addEdge(14,100)
+g.addEdge(100,19)
+g.addEdge(19,18)
+g.addEdge(19,17)
+
+g.findVertex(1)
+g.findVertex(12)
+g.findVertex(13)
+g.findVertex(14)
+g.findVertex(100)
+
+	
